70909261796b4c7c57c6dff6895c4a8d79ffba71
Brandon Carr-Ansah <puck4life@outlook.com>
2024-11-01 22:01:54 -0400

    add test player 2 win

--- RockPaperScissorsTests/RockPaperScissorsTests.cpp
+++ RockPaperScissorsTests/RockPaperScissorsTests.cpp
@@ -25,6 +25,12 @@ namespace RockPaperScissorsTests
 			Assert::AreEqual("Player1", result);
 		}
 
+		TEST_METHOD(TestPlayer2WinsRock)
+		{
+			const char* result = RockPaperScissors("Scissors", "Rock");
+			Assert::AreEqual("Player2", result);
+		}
+
 		TEST_METHOD(TestInvalidInput)
 		{
 			const char* result = RockPaperScissors("Water", "Rock");
