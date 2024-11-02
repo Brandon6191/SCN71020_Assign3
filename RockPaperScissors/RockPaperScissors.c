36a5430082411620894779cd62be6a9fa416cb27
Brandon Carr-Ansah <puck4life@outlook.com>
2024-11-01 21:58:47 -0400

    add check for who won

--- RockPaperScissors/RockPaperScissors.c
+++ RockPaperScissors/RockPaperScissors.c
@@ -12,6 +12,16 @@ const char* RockPaperScissors(const char* player1, const char* player2) {
     if (strcmp(player1, player2) == 0) {
         return "Tie";
     }
+
+    //Checks who won
+    if ((strcmp(player1, "Rock") == 0 && strcmp(player2, "Scissors") == 0) ||
+        (strcmp(player1, "Paper") == 0 && strcmp(player2, "Rock") == 0) ||
+        (strcmp(player1, "Scissors") == 0 && strcmp(player2, "Paper") == 0)) {
+        return "Player1";
+    }
+    else {
+        return "Player2";
+    }
 }
 
 int main() {
