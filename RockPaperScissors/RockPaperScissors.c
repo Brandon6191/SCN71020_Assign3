8958f49195f7e78d29f3e1276616413ad6582ce1
Brandon Carr-Ansah <puck4life@outlook.com>
2024-11-01 21:52:25 -0400

    add test pass

--- RockPaperScissors/RockPaperScissors.c
+++ RockPaperScissors/RockPaperScissors.c
@@ -2,6 +2,10 @@
 #include <string.h>
 
 const char* RockPaperScissors(const char* player1, const char* player2) {
+    //Checks for tie
+    if (strcmp(player1, player2) == 0) {
+        return "Tie";
+    }
 }
 
 int main() {
