9d8da5a7cb87165bba096699cd2c14461af2e16e
Brandon Carr-Ansah <puck4life@outlook.com>
2024-11-01 21:48:20 -0400

    Add project files.

---
+++ RockPaperScissors/RockPaperScissors.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+const char* RockPaperScissors(const char* player1, const char* player2) {
+}
+
+int main() {
+
+    return 0;
+}
---
+++ RockPaperScissorsTests/RockPaperScissorsTests.cpp
@@ -0,0 +1,22 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+extern "C" {
+#include "../RockPaperScissors/RockPaperScissors.h"
+}
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace RockPaperScissorsTests
+{
+	TEST_CLASS(RockPaperScissorsTests)
+	{
+	public:
+
+		TEST_METHOD(TestTieCase)
+		{
+			const char* result = RockPaperScissors("Rock", "Rock");
+			Assert::AreEqual("Tie", result);
+		}
+	};
+}
